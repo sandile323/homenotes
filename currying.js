42e1790cf66edb5b5af4a8ed8452a3f8597e7781
sandile323 <47978346+sandile323@users.noreply.github.com>
2023-11-10 16:10:12 +0200

    :memo: Update currying.js

--- currying.js
+++ currying.js
@@ -1,5 +1,26 @@
-function sum(a,b,c) {
-    return a + b + c
-}
+/***********************************************************************************************************
+ ***********************************************************************************************************
 
-console.log()
+ Currying is a process of functional programming where we transforms a function with multiple arguments
+ into a sequence of nesting functions taking 1 argument at a time
+
+ function f(a,b,c) is transformed to f(a)(b)(c
+    )
+************************************************************************************************************
+************************************************************************************************************/
+
+const sum = (a, b, c) => a + b + c;
+
+console.log(sum(1, 2, 3));
+
+const curry = (fn) => (a) => (b) => (c) => fn(a, b, c);
+
+const curriedSum = curry(sum);
+
+console.log(curriedSum(1)(2)(3));
+
+const add2 = curriedSum(2)
+const add3 = add2(3)
+const add5 = add3(5)
+
+console.log(add5)
